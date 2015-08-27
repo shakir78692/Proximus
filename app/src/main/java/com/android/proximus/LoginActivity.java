4c3e5af1c6c2b562e668bc8cd20fcc8e90d1175f
shakir78692 <shakir.kasmani@gmail.com>
2015-08-28 00:30:16 +0530

    Initial commit

---
+++ app/src/main/java/com/android/proximus/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.android.proximus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
